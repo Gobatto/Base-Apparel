14b72fa4627b120e5bfa8a92098006788416bac9
Raphael Gobatto Laranjeira <raphagobatto@hotmail.com>
2023-01-12 17:47:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+var big-image = {
+    image: "https://i.pinimg.com/564x/13/b2/40/13b2400a3aeb0f2df3f5ab90a80a09b1.jpg"}
+
+var small-image = {
+        image: "https://i.pinimg.com/564x/13/b2/40/13b2400a3aeb0f2df3f5ab90a80a09b1.jpg"}
